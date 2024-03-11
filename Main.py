5b56197467d0e1aa8244efc8a0d7fd1776b27d18
Matthewtizon <johnmatthewtizon300@gmail.com>
2024-03-12 07:17:07 +0800

    Initial commit

---
+++ Main.py
@@ -0,0 +1,26 @@
+#define who is the richest person in the 3
+person_1 = input("What is your name")
+value_of_person_1 = input("How much money do you have: ")
+
+
+person_2 = input("What is your name")
+value_of_person_2 = input("How much money do you have: ")
+
+
+person_3 = input("What is your name")
+value_of_person_3 = input("How much money do you have: ")
+
+
+
+
+if float(value_of_person_1) > float(value_of_person_2) and float(value_of_person_1) > float(value_of_person_3):
+    print(f"then {person_1} is the richest among the 3")
+
+
+
+
+
+
+
+
+
